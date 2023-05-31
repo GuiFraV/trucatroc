dabc8b0b3ffc3e3e553ee8577b13c2f03f487704
Your Name <you@example.com>
2023-05-31 09:29:28 +0200

    first push

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,26 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "C:\\Users\\GuiFra\\Desktop\\Test Trucatroc\\app\\layout.tsx": [
+      "C:\\Users\\GuiFra\\Desktop\\Test Trucatroc\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "C:\\Users\\GuiFra\\Desktop\\Test Trucatroc\\app\\globals.css"
+    ]
+  },
+  "cssModules": {
+    "C:\\Users\\GuiFra\\Desktop\\Test Trucatroc\\app\\create\\page": [
+      "C:\\Users\\GuiFra\\Desktop\\Test Trucatroc\\app\\globals.css",
+      "C:\\Users\\GuiFra\\Desktop\\Test Trucatroc\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "C:\\Users\\GuiFra\\Desktop\\Test Trucatroc\\app\\page": [
+      "C:\\Users\\GuiFra\\Desktop\\Test Trucatroc\\app\\globals.css",
+      "C:\\Users\\GuiFra\\Desktop\\Test Trucatroc\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "C:\\Users\\GuiFra\\Desktop\\Test Trucatroc\\app\\register\\page": [
+      "C:\\Users\\GuiFra\\Desktop\\Test Trucatroc\\app\\globals.css",
+      "C:\\Users\\GuiFra\\Desktop\\Test Trucatroc\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "C:\\Users\\GuiFra\\Desktop\\Test Trucatroc\\app\\login\\page": [
+      "C:\\Users\\GuiFra\\Desktop\\Test Trucatroc\\app\\globals.css",
+      "C:\\Users\\GuiFra\\Desktop\\Test Trucatroc\\node_modules\\next\\font\\google\\target.css?{\"path\":\"app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
